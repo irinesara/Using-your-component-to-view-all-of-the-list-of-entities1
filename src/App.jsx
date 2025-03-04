20b4e01ed4f375d4ec92c96578ebc5726caf3caf
irinesara <irine.ixa@gmail.com>
2025-03-04 12:02:46 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import { useState } from 'react';
+import BookCard from "./components/bookcard";
+import './App.css';
+const books = [
+  {
+    id: 1,
+    image: 'https://tse2.mm.bing.net/th?id=OIP.IIIPIPZYMBOJgxFfWGRtBAHaHa&rs=1&pid=ImgDetMain',
+    name: 'The Great Adventure',
+    genre: 'Fiction',
+    author: 'Jane Doe',
+  },
+  {
+    id: 2,
+    image: 'https://i.thenile.io/r1000/9781496687197.jpg?r=6085750d133cf',
+    name: 'Mysteries of the Universe',
+    genre: 'Science',
+    author: 'John Smith',
+  },
+  {
+    id: 3,
+    image: 'https://images3.penguinrandomhouse.com/cover/9781933771106',
+    name: 'History Revisited',
+    genre: 'Non-Fiction',
+    author: 'Emily Johnson',
+  },
+  {
+    id: 4,
+    image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1700317474i/202368563.jpg',
+    name: 'Culinary Delights',
+    genre: 'Cooking',
+    author: 'Michael Brown',
+  },
+];
 
-function App() {
-  const books = [
-    {
-      id: 1,
-      image:
-        'https://tse2.mm.bing.net/th?id=OIP.IIIPIPZYMBOJgxFfWGRtBAHaHa&rs=1&pid=ImgDetMain',
-      name: 'The Great Adventure',
-      genre: 'Fiction',
-      author: 'Jane Doe',
-    },
-    {
-      id: 2,
-      image: 'https://i.thenile.io/r1000/9781496687197.jpg?r=6085750d133cf',
-      name: 'Mysteries of the Universe',
-      genre: 'Science',
-      author: 'John Smith',
-    },
-    {
-      id: 3,
-      image: 'https://images3.penguinrandomhouse.com/cover/9781933771106',
-      name: 'History Revisited',
-      genre: 'Non-Fiction',
-      author: 'Emily Johnson',
-    },
-    {
-      id: 4,
-      image:
-        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1700317474i/202368563.jpg',
-      name: 'Culinary Delights',
-      genre: 'Cooking',
-      author: 'Michael Brown',
-    },
-  ];
-
-  return <>{/* write code to display all the books using */}</>;
+export default function App() {
+  return (
+    <div style={{ padding: '32px', display: 'flex', gap: '16px', overflowX: 'auto' }}>
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          image={book.image}
+          name={book.name}
+          genre={book.genre}
+          author={book.author}
+        />
+      ))}
+    </div>
+  );
 }
-
-export default App;
